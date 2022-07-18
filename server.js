258d3f5d86237250550f550f03a9f3d254dcddf9
PSAF <70419270+JakePelrah@users.noreply.github.com>
2022-07-17 21:08:42 -0400

    load from genre folder

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 const express = require('express')
-const app = express()
-const port = 8080
+const cookieParser = require('cookie-parser')
+const fs = require('fs')
+const { send } = require('process')
 
+const port = 3000
+const app = express()
+
+
+
+app.use(cookieParser())
 app.use(express.static('public'))
+
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+
+app.get('/data', async (req, res) => {
+
+    let masterMapping = {}
+
+    try {
+
+        // get all genres
+        const genreFolders = await fs.promises.readdir('public/genres')
+        for (const genre of genreFolders) {
+            const mapping = await fs.promises.readFile(`public/genres/${genre}/mapping.json`, 'utf-8')
+            masterMapping[genre] = JSON.parse(mapping)
+        }
+        res.json(masterMapping)
+    }
+    catch (e) { console.log(e) }
+
+})
+
+
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
